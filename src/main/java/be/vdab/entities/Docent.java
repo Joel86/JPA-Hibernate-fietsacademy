491ea75bfd2a33037a92e7b756e2068d7804cfd7
Joel Vanherle <joel.vanherle@hotmail.com>
2017-10-13 15:40:58 +0200

    Added geslacht to docent entity

--- src/main/java/be/vdab/entities/Docent.java
+++ src/main/java/be/vdab/entities/Docent.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import be.vdab.enums.Geslacht;
 
 @Entity
 @Table(name = "docenten")
@@ -17,6 +21,8 @@ public class Docent implements Serializable {
 	private String familienaam;
 	private BigDecimal wedde;
 	private long rijksRegisterNr;
+	@Enumerated(EnumType.STRING)
+	private Geslacht geslacht;
 	protected Docent() {}
 	public long getId() {
 		return id;
@@ -47,6 +53,9 @@ public class Docent implements Serializable {
 	}
 	public void setRijksRegisterNr(long rijksRegisterNr) {
 		this.rijksRegisterNr = rijksRegisterNr;
+	}
+	public Geslacht getGeslacht() {
+		return geslacht;
 	}
 	public String getNaam() {
 		return voornaam + ' ' + familienaam;
