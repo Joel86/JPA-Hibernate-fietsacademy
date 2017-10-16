5d373e82842385830566236899648bfc599b5d3f
Joel Vanherle <joel.vanherle@hotmail.com>
2017-10-16 10:13:28 +0200

    Added docent toevoegen functionality

--- src/main/java/be/vdab/entities/Docent.java
+++ src/main/java/be/vdab/entities/Docent.java
@@ -6,6 +6,8 @@ import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,13 +18,21 @@ import be.vdab.enums.Geslacht;
 public class Docent implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
-	private long id;
+	@GeneratedValue(strategy=GenerationType.IDENTITY) private long id;
 	private String voornaam;
 	private String familienaam;
 	private BigDecimal wedde;
 	private long rijksRegisterNr;
 	@Enumerated(EnumType.STRING)
 	private Geslacht geslacht;
+	public Docent(String voornaam, String familienaam, BigDecimal wedde, 
+			Geslacht geslacht, long rijksRegisterNr) {
+		setVoornaam(voornaam);
+		setFamilienaam(familienaam);
+		setWedde(wedde);
+		setGeslacht(geslacht);
+		setRijksRegisterNr(rijksRegisterNr);
+	}
 	protected Docent() {}
 	public long getId() {
 		return id;
@@ -34,30 +44,61 @@ public class Docent implements Serializable {
 		return voornaam;
 	}
 	public void setVoornaam(String voornaam) {
+		if(!isVoornaamValid(voornaam)) {
+			throw new IllegalArgumentException();
+		}
 		this.voornaam = voornaam;
 	}
 	public String getFamilienaam() {
 		return familienaam;
 	}
 	public void setFamilienaam(String familienaam) {
+		if(!isFamilienaamValid(familienaam)) {
+			throw new IllegalArgumentException();
+		}
 		this.familienaam = familienaam;
 	}
 	public BigDecimal getWedde() {
 		return wedde;
 	}
 	public void setWedde(BigDecimal wedde) {
+		if(!isWeddeValid(wedde)) {
+			throw new IllegalArgumentException();
+		}
 		this.wedde = wedde;
 	}
 	public long getRijksRegisterNr() {
 		return rijksRegisterNr;
 	}
 	public void setRijksRegisterNr(long rijksRegisterNr) {
+		if(!isRijksRegisterNrValid(rijksRegisterNr)) {
+			throw new IllegalArgumentException();
+		}
 		this.rijksRegisterNr = rijksRegisterNr;
 	}
 	public Geslacht getGeslacht() {
 		return geslacht;
 	}
+	public void setGeslacht(Geslacht geslacht) {
+		this.geslacht = geslacht;
+	}
 	public String getNaam() {
 		return voornaam + ' ' + familienaam;
+	}
+	public static boolean isVoornaamValid(String voornaam) {
+		return voornaam != null && !voornaam.trim().isEmpty();
+	}
+	public static boolean isFamilienaamValid(String familienaam) {
+		return familienaam != null && !familienaam.trim().isEmpty();
+	}
+	public static boolean isWeddeValid(BigDecimal wedde) {
+		return wedde != null && wedde.compareTo(BigDecimal.ZERO) >= 0;
+	}
+	public static boolean isRijksRegisterNrValid(long rijksRegisterNr) {
+		long getal = rijksRegisterNr / 100;
+		if(rijksRegisterNr / 1_000_000_000 < 50) {
+			getal += 2_000_000_000;
+		}
+		return rijksRegisterNr % 100 == 97 - getal % 97;
 	}
 }
--- src/main/java/be/vdab/repositories/DocentRepository.java
+++ src/main/java/be/vdab/repositories/DocentRepository.java
@@ -10,4 +10,7 @@ public class DocentRepository {
 	public Optional<Docent> read(long id, EntityManager entityManager) {
 		return Optional.ofNullable(entityManager.find(Docent.class, id));
 	}
+	public void create(Docent docent, EntityManager entityManager) {
+		entityManager.persist(docent);
+	}
 }
